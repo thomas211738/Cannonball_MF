cd635bba76235352f266a2afc99e22fb46b56e78
MarkoNeuro <68203818+MarkoNeuro@users.noreply.github.com>
2024-02-08 17:09:10 +0000

    saved data in data.js by playing game (server worked)

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id",
+  apiKey: "AIzaSyCwvUppqckqsXqKfs-Tj_jz8yRSx0nFq1c",
+  authDomain: "test-project-3b04c.firebaseapp.com",
+  projectId: "test-project-3b04c",
+  storageBucket: "test-project-3b04c.appspot.com",
+  messagingSenderId: "629661431349",
+  appId: "1:629661431349:web:43dcfdb459ab68c9efe37c",
 };
   
 export default firebaseConfig;
